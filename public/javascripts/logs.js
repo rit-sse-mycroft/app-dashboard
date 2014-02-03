e67c8ec60c1f6dec60fd696152491638d767297b
kristenmills <kristen@kristen-mills.com>
2014-02-02 19:25:11 -0500

    added coloring for logs

--- public/javascripts/logs.js
+++ public/javascripts/logs.js
@@ -1,10 +1,18 @@
 var socket = io.connect('http://localhost:7777');
+var classes = {
+  DEBUG: 'text-muted',
+  INFO: 'text-info',
+  WARNING: 'text-warning',
+  ERROR: 'text-danger',
+  EXCEPTION: 'text-danger',
+  WTF: 'text-danger'
+}
 socket.on('initial logs', function(data){
   for(i = 0; i < data.length; i++){
-    $('#logs').prepend('<p class="' + data[i]['level'] + '">' + data[i]['message'] + '</p>');
+    $('#logs').prepend('<p class="' + data[i]['level'] + ' ' + classes[data[i]['level']] + '">' + data[i]['message'] + '</p>');
   }
 });
 
 socket.on('log', function(data){
-  $('#logs').prepend('<p class="' + data['level'] + '">' + data['message'] + '</p>');
+  $('#logs').prepend('<p class="' + data['level'] + ' ' + classes[data['level']] + '">' + data['message'] + '</p>');
 });
